eb33da16cf589d73ee4198bb922343e8ecb6543e
sumant murke <sumantmurke@gmail.com>
2014-04-12 19:26:43 -0700

    Last commit with some validations

--- library/src/main/resources/assets/js/app.js
+++ library/src/main/resources/assets/js/app.js
@@ -3,11 +3,16 @@ $(":button").click(function() {
     var isbn = this.id;
     alert('About to report lost on ISBN ' + isbn);
     
-    var text = jQuery.parseJSON($.ajax({
+    var msg = jQuery.parseJSON($.ajax({
 		type: "put",
 		async: false,
 		cache: false,
 		url : "/library/v1/books/" + isbn + "?status=lost",
+		  success: function(response) {
+		        alert(JSON.stringify(response));
+		        $('#'+isbn).prop("disabled",true);
+		        $('#'+isbn+'Status').html("lost");
+		    }
 	}).responseText);
-    alert( (text) ? 'Book ISBN ' + isbn + ' was reported lost.' : 'Failed to report Book ISBN ' + isbn + ' lost.');
+    alert( (msg) ? 'Book of ISBN ' + isbn + ' is lost.' : 'Cant report Book of ISBN ' + isbn + ' as lost due to some internal server error.');
 });
